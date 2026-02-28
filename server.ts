9faf201a763faefbcfa3e7246323ea22eaa0bd50
ChesterTheCatt <121646655+ChesterTheCatt@users.noreply.github.com>
2026-02-28 17:29:26 -0300

    feat: Configure server for external access
    Update `package.json` to simplify the `start` script.
    Modify `server.ts` to bind to `0.0.0.0` and use the `PORT` environment variable, enabling the server to be accessible externally.

--- server.ts
+++ server.ts
@@ -6,8 +6,8 @@ import { Server as SocketIOServer } from 'socket.io';
 import { initGameServer } from './server/game';
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = parseInt(process.env.PORT || "3000", 10);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
